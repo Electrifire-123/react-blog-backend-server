cdbaf1fe0ebb00c7fd11d9d95b7e0f445b166ae8
Electrifire-123 <rahullyadavv77@gmail.com>
2022-07-12 20:56:08 +0530

    json

--- detail/bollydetails.js
+++ detail/bollydetails.js
@@ -1,114 +1,114 @@
 const getBolly = (req, res) => {
   res.send([
     {
-      id: 1,
-      date: "January 23, 2022",
-      title:
+      "id": "1",
+      "date": "January 23, 2022",
+      "title":
         "Singer Sidhu Moose Wala Shot Dead. Canada-Based Gangster Did It, Say Cops",
-      Image:
+      "Image":
         "https://c.ndtvimg.com/2022-05/7l227qf4_moose-wala_625x300_29_May_22.jpg",
-      category: "Bollywood",
-      description:
+      "category": "Bollywood",
+      "description":
         "New Delhi: Congress leader and Punjabi singer Sidhu Moose Wala was shot dead on Sunday, a day after his security was withdrawn by the state police. However, it is to be pondered upon, what made the Punjabi singing fame surrounded by controversies Born on 17 June 1993, Moosewala, whose real name is Shubhdeep Singh Sidhu, belongs to the Moosewala village which is situated in Punjab's Mansa district. Moose Wala enjoyed stardom only a few could imagine at a young age in the twenties of his life. He learned music during his college days and after completing his education, he moved to Canada.However, being widely known for his 'gangster rap', hit the headlines for all the wrong reasons.He was widely popular among the youth having 6.9 million followers on Instagram. However, his stardom was often overtaken by the controversies surrounding him for promoting drugs and violence through his songs.For instance, one of his songs titled 'Jatti Jeone Morh Wargi' came under the scanner as it made a reference to the 18th-century Sikh warrior Mai Bhago.Many FIRs were also registered against Moose Wala for promoting violence and hurting the religious sentiments of the Sikh community. However, he had issued an apology later on after people slammed him for portraying the Sikh warrior in a poor light.Moose Wala was booked after the pictures of him with an AK-47 went viral on social media during the COVID pandemic.Later, he was also booked by the Punjab police under the Arms Act after being charged with the promotion of gun culture in 2020. The action was taken for one of his songs Panj Goliyan",
     },
     {
-      id: 2,
-      date: "January 28, 2022",
-      title:
+      "id": "2",
+      "date": "January 28, 2022",
+      "title":
         "Steven Spielberg had introduced Aamir Khan to Tom Hanks as ‘James Cameron of India’; Tom revealed that he has seen 3 Idiots as many as 3 times!",
-      Image:
+      "Image":
         "https://stat1.bollywoodhungama.in/wp-content/uploads/2022/05/Steven-Spielberg-had-introduced-Aamir-Khan-to-Tom-Hanks-as-%E2%80%98James-Cameron-of-India%E2%80%99-Tom-revealed-that-he-has-seen-3-Idiots-as-many-as-3-times-2.jpg",
-      category: "Bollywood",
-      description:
+      "category": "Bollywood",
+      "description":
         "It has recently come to light that Tom Hanks is an admirer of Aamir Khan as well. Aamir Khan happened to meet him somewhere in 2014 or 2015 when he flew down to Germany to have a meeting with acclaimed filmmaker Steven Spielberg. It so happened that Aamir Khan wished to remake Forrest Gump (1994) whose rights rested with its director Robert Zemeckis. Robert, however, refused to meet Aamir. Aamir tried a lot to set up the meeting but in vain. Left with no choice, he decided to meet Steven Spielberg, who was then shooting for his film Bridge Of Spies (2015). Interestingly, the film starred Tom Hanks, who also starred in Forrest Gump and whose role Aamir wanted to essay in the remake. Robert Zemeckis held Spielberg in high regard. As a result, Aamir felt that if Steven Spielberg would convince Robert, the latter would part with the remake rights of Forrest Gump. Aamir Khan reached the sets of Bridge Of Spies and met Steven Spielberg along with Tom Hanks. Steven Spielberg introduced Aamir to Tom Hanks as ‘James Cameron of India’. This is because according to the filmmaker, just like how James Cameron breaks his own box office records, so does Aamir! Tom Hanks, however, made it clear that he knows Aamir very well. He also admitted that he loved Aamir’s 2009 film 3 Idiots and that he has seen it as many as 3 times! Aamir Khan then told Steven Spielberg about his predicament about Robert Zemeckis. To which Spielberg told Aamir that his relationship with Zemeckis was like that of a father and a son. And like most fathers and sons, they don’t always get along well! He also revealed to Aamir that Robert Zemeckis was refusing to meet the superstar as he didn’t want to say ‘no’ to Aamir’s face. Nevertheless, Aamir Khan kept on trying. At last, he won the remake rights. The remake of Forrest Gump came to be known as Laal Singh Chaddha. It is all set to release on August 11, 2022, and is one of the most awaited films of the year.",
     },
 
     {
-      id: 3,
-      date: "May 23, 2022",
-      title:
+      "id": "3",
+      "date": "May 23, 2022",
+      "title":
         "FAKE photo of Shah Rukh Khan, Salman Khan and Hrithik Roshan from Karan Johar's 50th birthday bash goes viral on the internet",
-      Image:
+      "Image":
         "https://static.toiimg.com/thumb/msid-91868025,width-1070,height-580,overlay-toi_sw,pt-32,y_pad-40,resizemode-75,imgsize-22362/91868025.jpg",
-      category: "Bollywood",
-      description:
+      "category": "Bollywood",
+      "description":
         "Karan Johar’s 50th birthday bash was a star-studded affair with the who’s who of Bollywood making their presence felt. Pictures and videos from the grand party are still doing the rounds on social media. playing near you in IMAX then this is the next best way to watch it. Fucken @RRRMovie is insane.When the team of RRR responded to thank Oswalt, he replied, “You guys are out of your f**king minds, you should not be allowed to make films, and I can’t wait to see what you do next.RR is a period drama starring Ram Charan and NTR Jr and follows two revolutionaries - Alluri Sitarama Raju and Komaram Bheem as they go up against the British rule in India. The film was released on March 24 this year. Since then, it has garnered significant buzz. It also kickstarted a debate on the way pan-India films are perceived. These Bollywood superstars coming together for a picture like this is surely a sight for sore eyes. However, much to the disappointment of their fans, the photo is an edited one and is clearly a fake.",
     },
     {
-      id: 4,
-      date: "January 23, 2022",
-      title:
+      "id": "4",
+      "date": "January 23, 2022",
+      "title":
         "Shah Rukh Khan, Salman Khan and Madhuri Dixit's selfie has fans recalling Hum Tumhare Hai Sanam",
-      Image:
+      "Image":
         "https://www.instagram.com/p/CeEa3N2PUjt/?utm_source=ig_embed&ig_rid=5fad3e1b-493e-4c8e-b143-6b38265d42dc",
-      category: "Bollywood",
-      description:
+      "category": "Bollywood",
+      "description":
         "Talking to Bollywood Hungama about her past films, she said, “I was not happy. Sometimes you work against your wishes, just because you have to keep working. When the thought of out of sight is out of mind takes over, you don't have many choices. Today it is quite easy for me to sit and say that instead of doing those films I should have learned kung fu in China or anything else. Today it is easy for me to say that. But it wasn't the same 6-7 years ago. Back then I was just trying to understand myself and wondering what happened. My first film did well and everyone even appreciated it. But why weren't certain choices given to me after that? Then I get the answer that you should do the kind of films which have songs, and if the song is a hit, the film also works. But I didn't think that way. It worked for somebody, but may not work for me. But you still want to try because somebody is saying, either someone who is experienced or someone from your team. They come from a  place where they mean well to you.She further said, “Then you are told that only when you work with certain names, and that you need to get more films with the really big stars to be out there. Now, that may also be true. But I did that, but it didn't work for me. One of them had a very good role also. I am talking about Kaabil but unfortunately, that was also like ‘aapka role usme utna hi tha (you had a small role). But I could not figure out why people were thinking like that. Why aren't they thinking about how I performed in that film. We often talk about films made outside of our country where big stars do small roles in big films. Anyway, it is easy to talk about this today. But back then it sounded like you are complaining about every trivial thing. It is better to take a breather and reshuffle everything.",
     },
     {
-      id: 5,
-      date: "January 23, 2022",
-      Image:
+      "id": "5",
+      "date": "January 23, 2022",
+      "Image":
         "https://stat1.bollywoodhungama.in/wp-content/uploads/2022/05/EXCLUSIVE-Yami-Gautam-reveals-she-was-told-to-work-with-big-stars-but-Hrithik-Roshan-starrer-Kaabil-did-not-work-for-her.jpeg",
-      title:
+      "title":
         "EXCLUSIVE: Yami Gautam reveals she was told to “work with big stars” but Hrithik Roshan starrer Kaabil did not work for her",
-      category: "Bollywood",
-      description:
+      "category": "Bollywood",
+      "description":
         "Akshay Kumar and Manushi Chhillar have kick-started promotions for their upcoming movie Prithviraj, which will be released on June 3. In a recent appearance on a comedy reality show, Akshay was all praise for Manushi Chhillar’s sharp memory. Manushi Chhillar also reciprocated by complimenting Akshay Kumar.In the reality show, Akshay Kumar said, If I talk about Manushi, she has such a sharp memory. She used to memorise all the dialogues, hers, mine as well as those of other actors. She could remember the toughest of words. The duo were also joined by the director, Chandraprakash Dwivedi, for the promotion of the movie on the reality show. He spoke in-depth about the upcoming release. The movie also stars Sanjay Dutt, Sonu Sood, Ashutosh Rana, and Sakshi Tanwar in pivotal rolesManushi also said that even though she loves Akshay Kumar’s comedy movies, she strongly feels that Prithviraj is his best movie to date. She said, I am a big fan of sir’s comedy and the few comedy films that sir has done. But, after watching Prithviraj, I can definitely say that Prithviraj is my favourite film of sir.",
     },
     {
-      id: 6,
-      date: "January 23, 2022",
-      title:
+      "id": "6",
+      "date": "January 23, 2022",
+      "title":
         "It's official! Hrithik Roshan introduced Saba Azad as his girlfriend at Karan Johar's bash",
-      Image:
+      "Image":
         "https://filmfare.wwmindia.com/content/2022/may/hrithikroshansabaazad41653633774.jpg",
-      category: "Bollywood",
-      description:
+      "category": "Bollywood",
+      "description":
         "Talks of Hrithik Roshan and Saba Azad’s rumoured relationship have been doing the rounds for a while. But now, the two have made it red carpet official by appearing together at Karan Johar's grand birthday bash. Hrithik and Saba arrived at the party on theme in their black outfits and made for a stunning pair. Turns out, after gracing the red carpet, Hrithik also introduced Saba Azad as his girlfriend to fellow attendeesAs per reports on a leading news portal, Hrithik Roshan was by Saba Azad's side the whole evening and went on to introduce her as his girlfriend to guests present at the bash. A source told the portal, Hrithik and Saba “didn’t leave each other’s side and were holding hands throughout the party.he couple (now that we can finally call them that) set the red carpet on fire as they posed for paps. Later, actress Preity Zinta also shared a picture with Hrithik and Saba. Take a look at their unmissable picFor the uninitiated, Hrithik Roshan and Saba Azad have been spending a lot of time together and needless to say, they’ve been the talk of the town. While they are yet to make an announcement to confirm their relationship, it cannot get more obvious than this.Recently, Hrithik and Saba also attended a birthday dinner together with the Roshan family. Hrithik’s father Rakesh Roshan and mother Pinkie were present at the intimate family gathering. Meanwhile, Karan Johar’s bash which took place in Mumbai on May 25 was attended by the most popular Bollywood stars including Aishwarya Rai Bachchan and Abhishek Bachchan, Katrina Kaif and Vicky Kaushal, Anushka Sharma, Ranveer Singh, Ranbir Kapoor and others.",
     },
     {
-      id: 7,
-      date: "January 23, 2022",
-      title:
+      "id": "7",
+      "date": "January 23, 2022",
+      "title":
         "Inside pictures from Karan Johar’s 50th birthday: Anushka Sharma and Ranbir Kapoor reunite",
-      Image:
+      "Image":
         "https://filmfare.wwmindia.com/content/2022/may/vickykaushal41653570327.jpg",
-      category: "Bollywood",
-      description:
+      "category": "Bollywood",
+      "description":
         "Karan Johar’s 50th birthday bash is the talk of B-town. The event was attended by A-listers such as Salman Khan, Aamir Khan, Kareena Kapoor Khan, Saif Ali Khan, Kajol, Rani Mukerji, Shahid Kapoor, Mira Kapoor, Anushka Sharma, Gauri Khan, Hrithik Roshan, Ranveer Singh, Ranbir Kapoor among others.After the red-carpet pictures of celebrities riled up a storm, we are now looking at the inside pictures from the event. These pictures candidly show our favourite stars having the time of their lives. As Katrina Kaif and Vicky Kaushal dish out couple goals, Ranbir Kapoor and Anushka Sharma reunite. ",
     },
     {
-      id: 8,
-      date: "January 23, 2022",
-      title:
+      "id": "8",
+      "date": "January 23, 2022",
+      "title":
         "Bollywood Alia Bhatt finally reacts to viral video of Ranbir Kapoor cradling a baby; cries 'happy tears'",
-      Image:
+      "Image":
         "https://st1.bollywoodlife.com/wp-content/uploads/2022/05/Alia-Ranbir-baby-video.jpg?impolicy=Medium_Widthonly&w=800",
-      category: "Bollywood",
-      description:
+      "category": "Bollywood",
+      "description":
         "A video of Ranbir Kapoor cradling a baby had gone viral on the internet. In the video, Ranbir was seen carrying the little one in his arms, kisses on the head and continues to play with the baby. The fan shared the video on Instagram and wrote, Many social media users started tagging Alia Bhatt after watching the video. And the viral video of Ranbir's has finally got Alia's much-needed attention, and it got her happy tears.",
     },
     {
-      id: 9,
-      date: "January 23, 2022",
-      title:
+      "id": "9",
+      "date": "January 23, 2022",
+      "title":
         "Bollywood Deepika Padukone looks jaw droppingly stunning in latest photos",
-      Image:
+      "Image":
         "https://www.pinkvilla.com/imageresize/deepika_69.jpg?width=752&format=webp&t=pvorg",
-      category: "Bollywood",
-      description:
+      "category": "Bollywood",
+      "description":
         "Deepika Padukone never fails to impress us with her style. The stunning actress is an epitome of grace and charm. Deepika is the highest paid actress in the country today and has made it to the Top 5 of Forbes List of Richest Indian Celebs. The actor was featured in TIME magazine’s 100 Most Influential People List and is one of the few Indian actors to have attended coveted events abroad. ",
     },
     {
-      id: 10,
-      date: "January 23, 2022",
-      title:
+      "id": "10",
+      "date": "January 23, 2022",
+      "title":
         "Bollywood Aishwarya Rai Bachchan, Abhishek Bachchan and daughter Aaradhya return ",
-      Image:
+      "Image":
         "https://www.pinkvilla.com/imageresize/abhishek%20bachchan%20aishwarya%20aaradhya%20%282%29.JPG?width=752&format=webp&t=pvorg",
-      category: "Bollywood",
-      description:
+      "category": "Bollywood",
+      "description":
         "Aishwarya Rai Bachchan, Abhishek Bachchan and daughter Aaradhya returned to Mumbai today after ringing in 2019 abroad. Abhishek and Aishwarya shared photos from their New Year getaway as the family basked in the sun. At the airport, daughter Aaradhya was engaged in a conversation with her parents and looked cute as a button in a pink dress.",
     },
   ]);
